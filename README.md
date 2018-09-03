5eed825665b4fadc9f6824ecd2d8707a700633ef
Shinan Liu <shinan@Shinans-MacBook-Pro.local>
2018-09-03 19:50:11 -0400

    last commit for this prject

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## iosHelloWorld
 ### simple changing text color, view color, and displaying text field text in label
+<img src= width=200><br>
 
+`TODO://` In the User stories section below, add an `x` between the `-[ ]` like this `- [x]` for any user story you complete. (☝️ Remove this paragraph after after checking off user stories)
 
 ### Required User Stories
 - [ ] 1. User sees custom text in a label - Hello from {name}!
--- iosHelloWorld/ViewController.swift
+++ iosHelloWorld/ViewController.swift
@@ -13,21 +13,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textLabel: UILabel!
     
+    @IBAction func onTapped(_ sender: Any) {
+        print("tapped")
+        textField.text = ""
+        textLabel.text = "Filler text"
+        textLabel.textColor = UIColor.black
+        view.backgroundColor = UIColor.green
+        
+    }
     @IBAction func changeText(_ sender: Any) {
         let text = textField.text
-        textLabel.text = text
+        if (textField.text?.isEmpty)!{
+            textLabel.text = "Filler text"
+        }else{
+            textLabel.text = text
+        }
+        self.view.endEditing(true)
         
-    
+      
     }
+    
+   
     @IBAction func changeView(_ sender: Any){
         view.backgroundColor = UIColor.blue
+        
+        self.view.endEditing(true)
+      
     }
     @IBAction func changeColor(_ sender: Any){
         textLabel.textColor = UIColor.orange
+        self.view.endEditing(true)
         
     }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.green
+        if (textField.text?.isEmpty)!{
+            textLabel.text = "Filler text"
+        }
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
 
